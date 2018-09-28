770b8c658dbef1e0f45f2b8f2b3e7b52d337d8a5
AlexeyYan <33554901+AlexeyYan@users.noreply.github.com>
2018-09-28 18:25:14 +0300

    1

--- Server.py
+++ Server.py
@@ -3,7 +3,7 @@ import websockets
 
 async def echo(websocket, path):
     name=await websocket.recv()
-    print("{"+name+"}"}
+    print("{"+name+"}")
     await websocket.send(name.upper())
 
 asyncio.get_event_loop().run_until_complete(websockets.serve(echo,'',os.environ.get('PORT')))
