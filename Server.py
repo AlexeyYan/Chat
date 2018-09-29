0ef7165f068864a2f9f26b7dab0137d1f71fe1fb
AlexeyYan <33554901+AlexeyYan@users.noreply.github.com>
2018-09-29 22:33:08 +0300

    Update dynamic port

--- Server.py
+++ Server.py
@@ -38,5 +38,5 @@ application = tornado.web.Application([
 
 if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8080)
+    http_server.listen(os.environ.get('PORT', 5000))
     tornado.ioloop.IOLoop.instance().start()
