585fc209dac98a2c30287327cd6697300fc92781
AlexeyYan <yanovichaleksey@gmail.com>
2018-10-24 01:29:05 +0300

    Add sqlalchemy demo 2

--- db_models.py
+++ db_models.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from db_handler import base
 from hashlib import md5
 import time
+from datetime import datetime
 
 class User(base):
     __tablename__='Users'
@@ -11,6 +12,7 @@ class User(base):
     name=Column(String(40), index=True, unique=True)
     passwd=Column(String(30))
     key=Column(String(128))
+    messages=relationship('Message', backref='author', lazy='dynamic')
 
     def set_passwd(self, passwd):
         self.passwd=md5(str(passwd).encode()).hexdigest()
@@ -21,3 +23,17 @@ class User(base):
     def set_key(self):
         pre_key=self.passwd+str(time.time())
         self.key=md5(pre_key.encode()).hexdigest()
+
+    def __repr__():
+        return '<User {}>'.format(self.name)
+
+class Message(base):
+    __tablename__='Messages'
+
+    id=Column(Integer, primary_key=True)
+    text=Column(String(300))
+    user_id=Column(Integer, ForeignKey('user.id'))
+    timestamp=Column(DateTime, index=True, default=datetime.utcnow())
+
+   def  __repr__():
+       return '<Message: {}>'.format(self.text)
